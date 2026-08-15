42ed180da6a2524d106b56ddb998e1a70ce86be6
jeevana4587 <jeevanapavuluru4587@gmail.com>
2026-08-15 12:13:49 +0530

    made navbar more responsive and added logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,41 @@
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+import Logo from '../assets/logo.svg';
+
 function Navbar( ) {
+    const [open, setOpen] = useState(false);
+
     return (
-        <nav className="flex items-center justify-between w-full py-8 px-16
-        bg-black">
+        <nav className="relative flex items-center justify-between w-full py-8 px-6 sm:px-10 lg:px-16 bg-black">
            <div>
-            <p className="font bold text-2xl text-white">My Logo</p>
+             <img src={Logo} alt="Wireframe" className="w-full max-w-xs lg:max-w-sm px-8 lg:px-0"/>
             </div> 
 
-           <div className="flex gap-16">
-           <a href="#" className="hover:underline text-white text-lg">Home </a>
-           <p className="text-white text-lg">Services</p>
-           <p className="text-white text-lg">Contact us</p>
-           <p className="text-white text-lg">About us</p>
+           <div className="hidden md:flex gap-10 lg:gap-16">
+           <a href="#" className="hover:underline text-white text-lg">Home</a>
+           <a href="#" className="text-white text-lg">Services</a>
+           <a href="#" className="text-white text-lg">Contact us</a>
+           <a href="#" className="text-white text-lg">About us</a>
            </div>
 
            <div>
-           <button className="bg-orange-500 text-white px-7 py-2 rounded-lg">Login</button>
+           <button className="hidden md:block bg-orange-500 text-white px-7 py-2 rounded-lg">Login</button>
            </div>
+
+           <button className="md:hidden text-white" onClick={() => setOpen(!open)}>
+             {open ? <X size={28} /> : <Menu size={28} />}
+           </button>
+
+
+           {open && (
+        <div className="absolute top-full left-0 w-full bg-black flex flex-col items-center gap-6 py-8 md:hidden z-50">
+          <a href="#" className="text-white text-lg">Home</a>
+          <a href="#" className="text-white text-lg">Services</a>
+          <a href="#" className="text-white text-lg">Contact us</a>
+          <a href="#" className="text-white text-lg">About us</a>
+          <button className="bg-orange-500 text-white px-7 py-2 rounded-lg">Login</button>
+        </div>
+        )}
 
         </nav>
     );
